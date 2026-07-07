271c00424d8df48717e134d25400d8d56a1aaf68
Alyssa <alyssa.dubue25@gmail.com>
2026-07-07 13:49:26 -0600

    Added all components and features to app

---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+
+
+    return (
+    <header>
+        <div className="logo">
+            <p>Reddit Client Min</p>
+        </div>
+    </header>
+    );
+};
+
+export default Header;
